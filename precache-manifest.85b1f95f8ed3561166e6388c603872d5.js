15df2b22663d3425b590cea1c3ead3eb30625744
officialmd <officialmohamad@hotmail.com>
2026-01-04 23:15:56 -0500

    Updates

---
+++ precache-manifest.85b1f95f8ed3561166e6388c603872d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0b65e1de1c93f167284156ff9c01abd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d9c5c8fed0422b3d6d1",
+    "url": "/static/css/2.a5344151.chunk.css"
+  },
+  {
+    "revision": "bb888e6a73f7cec3ac07",
+    "url": "/static/css/main.a29bbaa0.chunk.css"
+  },
+  {
+    "revision": "5d9c5c8fed0422b3d6d1",
+    "url": "/static/js/2.4c36ac5a.chunk.js"
+  },
+  {
+    "revision": "66f552baae5060f6381422c58f93539e",
+    "url": "/static/js/2.4c36ac5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb888e6a73f7cec3ac07",
+    "url": "/static/js/main.c3183422.chunk.js"
+  },
+  {
+    "revision": "962b2687f11eff1e833b",
+    "url": "/static/js/runtime-main.bcb9ebb2.js"
+  }
+]);
